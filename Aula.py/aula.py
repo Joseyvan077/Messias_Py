960cbefa3bc417abbc6a2ab5b95aa66e6caeb838
Jose Yvan Rodrigues Marques dos Santos <Yvan@academicos.com>
2023-09-18 20:06:25 -0300

    Aula-18/09

---
+++ Aula.py/aula.py
@@ -0,0 +1,17 @@
+def multiplicar(N1, N2): 
+    return N1 * N2
+
+
+def somar(N1, N2):
+    return N1 + N2
+
+
+def dividir(N1, N2):
+    return N1 / N2
+
+
+print(f'multiplicar: {multiplicar(N1=2, N2=3)}')
+print(f'somar: {somar(N1=2, N2=3)}')
+print(f'dividir: {dividir(N1=2, N2=3)}')
+print("Fim DO Codigo")
+
